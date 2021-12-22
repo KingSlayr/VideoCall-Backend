66388630f9834e13fd85ad979259cb1b18550463
KingSlayr <ankitrastogi31200@gmail.com>
2021-12-22 22:31:21 +0530

    origin updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://connect-mee.herokuapp.com/",
+		origin: "https://connect-mee.herokuapp.com",
 		methods: [ "GET", "POST" ]
 	}
 })
