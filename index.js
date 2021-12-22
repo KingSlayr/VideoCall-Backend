3d456b40de3ce37eaa45e409a51657af8da59079
KingSlayr <ankitrastogi31200@gmail.com>
2021-12-22 22:30:03 +0530

    origin updated

--- index.js
+++ index.js
@@ -47,4 +47,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(process.env.PORT || 5000, () => console.log("server is running on port 5000"))
